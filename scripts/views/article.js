8d3d9bc676fe2f720745e385a4b496dbceb01c4d
mingxin.huang <mingxinstar.huang@gmail.com>
2015-08-09 09:12:34 +0800

    增加页面读取接口

--- scripts/views/article.js
+++ scripts/views/article.js
@@ -10,6 +10,7 @@ define(function (require) {
         template = require('template'),
 
         stories = require('collections/stories'),
+        page = require('models/readPage'),
         articleTmpl = require('text!templates/article.html');
 
     var article = Backbone.View.extend({
@@ -17,12 +18,23 @@ define(function (require) {
         initialize : function (article_id) {
             this.model = stories.get(article_id);
 
-            console.log('init article : ', article_id, this.model);
-
             this.render();
+
+            var modelJSON = this.model.toJSON();
+
+            if (this.model.get('url') && !this.model.get('text')) {
+                this.pageModel = new page(this.model.get('id'), this.model.get('url'));
+
+                this.listenTo(this.pageModel, 'change', this.renderPage);
+            }
         },
         render : function () {
             this.$el.html(template(articleTmpl, {data : this.model.toJSON()}));
+
+            return this;
+        },
+        renderPage : function () {
+            this.$('.article-content').html(this.pageModel.toJSON().content).removeClass('article-loading');
 
             return this;
         }
